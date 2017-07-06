a3e818ee56b4b321ca0f0621bdb1765381ffb624
Jake Avery <jakeavery@jakeavery.me>
2017-07-06 07:16:59 -0700

    Windows and buttons and parenting

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,17 +4,29 @@
 int main(int argc, char **argv) {
     QApplication app (argc, argv);
 
-    QPushButton button;
-    button.setText("Whaddupppp");
-    button.setToolTip("This is what's up");
+    QWidget firstWindow;
+    firstWindow.setFixedSize(300, 100);
+
+    QPushButton button1;
+    button1.setText("Whaddupppp");
+    button1.setToolTip("This is what's up");
+    button1.setParent(&firstWindow);
 
     QFont courierFont ("Courier", 24, 96, true);
-    button.setFont(courierFont);
+    button1.setFont(courierFont);
 
     QIcon materialIcon ("D:/Qt-Tutorial/HelloWorld/img/icon.png");
-    button.setIcon(materialIcon);
+    button1.setIcon(materialIcon);
 
-    button.show();
+    QPushButton button2 ("THIS is what's up", &firstWindow);
+
+    QFont smallerFont ("Helvetica", 8, -1, false);
+    button2.setFont(smallerFont);
+
+    QPushButton *anotherButton = new QPushButton("What is THIS though...", &firstWindow);
+    anotherButton->setGeometry(50, 50, 200, 50);
+
+    firstWindow.show();
 
     return app.exec();
 }
