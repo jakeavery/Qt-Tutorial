ce482548792541977914799fdba5a89c5a7c7e14
Jake Avery <jakeavery@jakeavery.me>
2017-07-05 22:23:56 -0700

    Working on icon

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,7 +4,16 @@
 int main(int argc, char **argv) {
     QApplication app (argc, argv);
 
-    QPushButton button ("Hello world!");
+    QPushButton button;
+    button.setText("Whaddupppp");
+    button.setToolTip("This is what's up");
+
+    QFont courierFont ("Courier", 24, 96, true);
+    button.setFont(courierFont);
+
+    QIcon materialIcon ("D:/icon.png");
+    button.setIcon(materialIcon);
+
     button.show();
 
     return app.exec();
